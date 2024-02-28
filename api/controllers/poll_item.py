6d7a76e6c056bc627ab01f069aab5ebe9e3f0dd3
Emilia Pyyny <emilia.pyyny@student.oulu.fi>
2024-02-28 23:44:56 +0200

    Update controllers

--- api/controllers/poll_item.py
+++ api/controllers/poll_item.py
@@ -1,17 +1,31 @@
 from prisma.models import PollItem      
 from api.models.poll_dtos import PollItemDto
 from flask import make_response, request, Blueprint, Response
+from flask_restful import Resource, Api
 from prisma.errors import UniqueViolationError
 from werkzeug.exceptions import BadRequest
 
-pollitem = Blueprint("pollitem", __name__)
+pollitem = Blueprint("pollitem", __name__, url_prefix="/pollitem")
+pollitem_api = Api(pollitem)
 
-#Creates a pollitem
-@pollitem.route("/pollitem/create", methods=["POST"])
-def create_pollitem():
-    pollitem_dto = PollItemDto.from_json(request.json)
-    try:
-        PollItem.prisma().create(data=pollitem_dto.to_insertable())
-    except UniqueViolationError:
-        raise BadRequest("username not unique")
-    return make_response({"description": pollitem_dto.description, "votes": pollitem_dto.votes})
+# Creates a poll item
+class PollItemCreate(Resource):
+    def post(self):
+        pollitem_dto = PollItemDto.from_json(request.json)
+        try:
+            PollItem.prisma().create(data=pollitem_dto.to_insertable())
+            poll_item = PollItem.prisma().find_first(where={"pollId": pollitem_dto.pollId, "description": pollitem_dto.description})
+        except UniqueViolationError:
+            raise BadRequest("username not unique")
+        return make_response({"description": poll_item.description, "votes": poll_item.votes, "id": poll_item.id})
+    
+class PollItemDetails(Resource):
+    def get(self, poll_item_id):
+        try:
+            poll_item = PollItem.prisma().find_first(where={"id": poll_item_id})
+        except UniqueViolationError:
+            raise BadRequest
+        return make_response({"description": poll_item.description, "votes": poll_item.votes})
+
+pollitem_api.add_resource(PollItemCreate, "/create")
+pollitem_api.add_resource(PollItemDetails, "/<poll_item_id:poll_item_id>")
