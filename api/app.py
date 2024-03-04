fef4751f130566e208268ef6d4b2c82a8194539d
Janne Kerola <janne.kerola@oulu.fi>
2024-03-04 11:29:56 +0200

    fix remaining warnings

--- api/app.py
+++ api/app.py
@@ -3,8 +3,8 @@
 from flask import Flask
 from werkzeug.exceptions import HTTPException
 from api.controllers.auth import auth
-from api.controllers.poll import poll
-from api.controllers.poll_item import pollitem
+from api.controllers.polls import polls
+from api.controllers.poll_items import poll_items
 from api.database import connect_to_db
 from api.middleware.error_handler import handle_exception
 from api.converters.poll import PollConverter
@@ -17,8 +17,8 @@ def create_app() -> Flask:
     app.url_map.converters["poll_id"] = PollConverter
     app.url_map.converters["poll_item_id"] = PollItemConverter
     app.register_blueprint(auth)
-    app.register_blueprint(poll)
-    app.register_blueprint(pollitem)
+    app.register_blueprint(polls)
+    app.register_blueprint(poll_items)
     app.register_error_handler(HTTPException, handle_exception)
     return app
 
